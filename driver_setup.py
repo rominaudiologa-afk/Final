33eb18b5e50f527f448c61e5d83e982dfb17b7c7
rominaudiologa-afk <rominaudiologa@gmail.com>
2026-07-07 21:55:29 -0300

    Add files via upload

---
+++ driver_setup.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+def get_driver():
+    # Setup de driver con versión automática
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
+    
+    driver.maximize_window()
+    return driver
